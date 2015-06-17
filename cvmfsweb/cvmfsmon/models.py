408c233d86090b0c42009dfb9f6c81ea91a4c5ad
Rene Meusel <rene@renemeusel.de>
2015-06-17 13:55:32 +0200

    introduce abstract Stratum model class

--- cvmfsweb/cvmfsmon/models.py
+++ cvmfsweb/cvmfsmon/models.py
@@ -1,20 +1,47 @@
 from django.db import models
 
-class Stratum0(models.Model):
-    name = models.CharField('Human readable name of the Stratum0 provider', max_length=100)
-    url  = models.URLField('Base URL for the Stratum Server')
+import cvmfs
+import urlparse
+
+class Stratum:
+    def connect_to(self, fqrn):
+        endpoint = urlparse.urljoin(self.get_base_url(), fqrn)
+        return cvmfs.open_repository(endpoint)
+
+    def is_stratum0(self):
+        return False
+
+    def is_stratum1(self):
+        return False
+
+
+class Stratum0(models.Model, Stratum):
+    name  = models.CharField('Human readable name of the Stratum0 provider', max_length=100)
+    url   = models.URLField('Base URL for the Stratum Server')
 
     def __unicode__(self):
         return self.name
 
+    def get_base_url(self):
+        return self.url
 
-class Stratum1(models.Model):
+    def is_stratum0(self):
+        return True
+
+
+class Stratum1(models.Model, Stratum):
     name  = models.CharField('Human readable name of the stratum 1 provider', max_length=100)
     url   = models.URLField('Base URL for the stratum 1 provider')
     alias = models.CharField('Textual identifier', max_length=20, unique=True)
 
     def __unicode__(self):
         return self.name
+
+    def get_base_url(self):
+        return self.url
+
+    def is_stratum1(self):
+        return True
 
 
 class Repository(models.Model):
