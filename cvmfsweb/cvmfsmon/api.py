ce212ac9eff80ef2dba54ac8dbbf9bf8a8cd8916
Rene Meusel <rene@renemeusel.de>
2015-06-17 13:56:02 +0200

    add Endpoint API resource

--- cvmfsweb/cvmfsmon/api.py
+++ cvmfsweb/cvmfsmon/api.py
@@ -1,7 +1,10 @@
-from django.conf.urls   import url
-from tastypie.resources import ModelResource
-from tastypie           import fields
-from cvmfsmon.models    import Stratum0, Stratum1, Repository
+from django.conf.urls    import url
+from tastypie.resources  import Resource, ModelResource
+from tastypie            import fields, http
+from tastypie.bundle     import Bundle
+from tastypie.exceptions import ImmediateHttpResponse
+from cvmfsmon.models     import Stratum0, Stratum1, Repository
+from tastypie.utils      import trailing_slash
 
 
 class Stratum1Resource(ModelResource):
@@ -35,3 +38,60 @@ class RepositoryResource(ModelResource):
             url(r"^(?P<resource_name>%s)/(?P<fqrn>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
         ]
 
+
+class EndpointStatus(object):
+    def __init__(self, stratum=None, fqrn=None):
+        if stratum and fqrn:
+            self._stratum = stratum
+            repo = self._stratum.connect_to(fqrn)
+            self.__read_status(repo)
+
+    def __read_status(self, repo):
+        self.fqrn             = repo.manifest.repository_name
+        self.revision         = repo.manifest.revision
+        self.last_modified    = repo.manifest.last_modified
+        self.whitelist_expiry = repo.retrieve_whitelist().expires
+        self.endpoint         = repo.endpoint
+
+    def make_endpoint_id(self):
+        stratum0 = self._stratum.is_stratum0()
+        kwargs = {}
+        kwargs['stratum_type']  = 'stratum0' if stratum0 else 'stratum1'
+        kwargs['stratum_alias'] = self._stratum.alias
+        kwargs['fqrn']          = self.fqrn
+        return kwargs
+
+
+class EndpointStatusResource(Resource):
+    fqrn             = fields.CharField(attribute='fqrn')
+    revision         = fields.IntegerField(attribute='revision')
+    last_modified    = fields.DateTimeField(attribute='last_modified')
+    whitelist_expiry = fields.DateTimeField(attribute='whitelist_expiry')
+    endpoint         = fields.CharField(attribute='endpoint')
+
+    class Meta:
+        resource_name   = 'endpoint'
+        detail_uri_name = 'endpoint_definition'
+        object_class    = EndpointStatus
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<stratum_type>[\w\d_.-]+)/(?P<fqrn>[\w\d_.-]+)/(?P<stratum_alias>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
+
+    def detail_uri_kwargs(self, bundle_or_obj):
+        is_bundle = isinstance(bundle_or_obj, Bundle)
+        obj = bundle_or_obj.obj if is_bundle else bundle_or_obj
+        return obj.make_endpoint_id()
+
+    def obj_get(self, bundle, **kwargs):
+        stratum_class = Stratum0 if   kwargs['stratum_type'] == 'stratum0' \
+                                 else Stratum1
+        stratum = stratum_class.objects.get(alias=kwargs['stratum_alias'])
+        return EndpointStatus(stratum, kwargs['fqrn'])
+
+    def obj_get_list(self, bundle, **kwargs):
+        raise ImmediateHttpResponse(response=http.HttpBadRequest())
+
+    def rollback(self, bundles):
+        pass
