54cd5d43fd42d0cc3d6ff426bd575db49def0a93
Rene Meusel <rene@renemeusel.de>
2015-06-19 15:27:49 +0200

    FIX: RepositoryResource.ToManyField(EndpointResource)

--- cvmfsweb/cvmfsmon/api.py
+++ cvmfsweb/cvmfsmon/api.py
@@ -23,42 +23,13 @@ class Stratum1Resource(ModelResource):
         ]
 
 
-class RepositoryResource(ModelResource):
-    endpoints = fields.ManyToManyField('cvmfsmon.api.EndpointResource', 'endpoints', null=True, full=True)
-
-    class Meta:
-        resource_name   = 'repository'
-        detail_uri_name = 'fqrn'
-        queryset        = Repository.objects.all()
-        allowed_methods = [ 'get' ]
-        excludes        = [ 'id' ]
-
-    def dehydrate_endpoints(self, bundle):
-        # TODO: not sure if that is the preferred way to connect this to the
-        #       EndpointResournce
-        esr = EndpointResource()
-        bundles = []
-        for s1 in bundle.obj.stratum1s.all():
-            endpoint = Endpoint(s1, bundle.obj.fqrn)
-            bundle = esr.build_bundle(obj=endpoint)
-            bundle = esr.full_dehydrate(bundle)
-            del bundle.data['fqrn'] # remove redundant fqrn field here
-            bundles.append(bundle)
-        return bundles
-
-    def prepend_urls(self):
-        return [
-            url(r"^(?P<resource_name>%s)/(?P<fqrn>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
-        ]
-
-
 class EndpointResource(ModelResource):
     class Meta:
         resource_name   = 'endpoint'
         detail_uri_name = 'fqrn'
         queryset        = Stratum.objects.all()
         allowed_methods = [ 'get' ]
-        fields          = [ 'endpoint' , 'fqrn' ]
+        fields          = [ 'endpoint' ]
 
     def prepend_urls(self):
         return [
@@ -66,7 +37,6 @@ class EndpointResource(ModelResource):
         ]
 
     def dehydrate(self, bundle):
-        bundle.data['fqrn']     = bundle.obj.fqrn
         bundle.data['endpoint'] = bundle.obj.make_endpoint(bundle.obj.fqrn)
         return bundle
 
@@ -78,11 +48,6 @@ class EndpointResource(ModelResource):
         kwargs['level'] = obj.level
         return kwargs
 
-    def __check_stratum_level(self, stratum_level):
-        if stratum_level not in [ 0 , 1 ]:
-            msg = "unknown stratum level %d" % stratum_level
-            raise ImmediateHttpResponse(response=http.HttpBadRequest(msg))
-
     def obj_get(self, bundle, **kwargs):
         fqrn = kwargs['fqrn']
         del kwargs['fqrn']
@@ -92,3 +57,33 @@ class EndpointResource(ModelResource):
 
     def obj_get_list(self, bundle, **kwargs):
         raise ImmediateHttpResponse(response=http.HttpBadRequest())
+
+
+class RepositoryResource(ModelResource):
+    endpoints = fields.ManyToManyField(EndpointResource, attribute=lambda bundle: RepositoryResource._wrap_endpoints(bundle), null=True, full=True)
+
+    class Wrapper:
+        def __init__(self, list):
+            self.list = list
+        def all(self):
+            return self.list
+
+    @staticmethod
+    def _wrap_endpoints(bundle):
+        stratums = []
+        for s in Stratum.objects.filter(level=1):
+            s.fqrn = bundle.obj.fqrn
+            stratums.append(s)
+        return RepositoryResource.Wrapper(stratums)
+
+    class Meta:
+        resource_name   = 'repository'
+        detail_uri_name = 'fqrn'
+        queryset        = Repository.objects.all()
+        allowed_methods = [ 'get' ]
+        excludes        = [ 'id' ]
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<fqrn>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
